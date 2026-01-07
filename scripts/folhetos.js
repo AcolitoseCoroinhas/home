7b0d38498ef04b479774513cf1bce5803c687f6c
Com.SantaGemmaGalgani-acolitosecoroinhas <acolitosecoroinhascsgg@gmail.com>
2026-01-07 18:16:18 -0300

    Update folhetos.js

--- scripts/folhetos.js
+++ scripts/folhetos.js
@@ -33,12 +33,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const div = document.createElement("div");
         div.classList.add("div-folhetos");
 
-        const corDoTexto = coresFolhetos[index % coresFolhetos.length];
-
         const corDoButton = coresButtons[index % coresFolhetos.length];
 
         div.innerHTML = `
-            <h1 class="text-folhetos" style="color: ${corDoTexto};">${folheto.data}</h1>
+            <h1 class="text-folhetos">${folheto.data}</h1>
             <a href="folhetos/${folheto.arquivo}" target="_blank">
                 <button class="button-folhetos" style="background-color: ${corDoButton};">Baixar</button>
             </a>
