f4691283370fecb00d061213c8c0689a0d5cf048
Com.SantaGemmaGalgani-acolitosecoroinhas <acolitosecoroinhascsgg@gmail.com>
2026-04-01 13:18:53 -0300

    Update folhetos.js

--- scripts/folhetos.js
+++ scripts/folhetos.js
@@ -3,16 +3,25 @@ document.addEventListener("DOMContentLoaded", () => {
     const folhetos = [
         
         { data: "Livr. Semana Santa", arquivo: "LIVRETOSEMANASANTA2026.pdf" },
+        { data: "12-Abr-26", arquivo: "12-abr-26.pdf" },
+        { data: "19-Abr-26", arquivo: "19-abr-26.pdf" },
+        { data: "26-Abr-26", arquivo: "26-abr-26.pdf" },
     ];
 
     const coresFolhetos = [
 
-        "rgb(255, 31, 31)"
+        "rgb(31, 173, 255)",
+        "rgb(245, 212, 67)",
+        "rgb(245, 212, 67)",
+        "rgb(245, 212, 67)"
     ];
 
     const coresButtons = [
 
-        "rgb(255, 31, 31)"
+        "rgb(31, 173, 255)",
+        "rgb(245, 212, 67)",
+        "rgb(245, 212, 67)",
+        "rgb(245, 212, 67)"
     ];
 
     const container = document.getElementById("folhetosContainer");
