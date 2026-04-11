ba482b3c4939a00dda848e4253ac34b20331a48c
Com.SantaGemmaGalgani-acolitosecoroinhas <acolitosecoroinhascsgg@gmail.com>
2026-04-11 14:40:12 -0300

    Update folhetos.js

--- scripts/folhetos.js
+++ scripts/folhetos.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const folhetos = [
         
-        { data: "Livr. Semana Santa", arquivo: "LIVRETOSEMANASANTA2026.pdf" },
         { data: "12-Abr-26", arquivo: "12-abr-26.pdf" },
         { data: "19-Abr-26", arquivo: "19-abr-26.pdf" },
         { data: "26-Abr-26", arquivo: "26-abr-26.pdf" },
@@ -10,7 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const coresFolhetos = [
 
-        "rgb(31, 173, 255)",
         "rgb(255, 213, 25)",
         "rgb(255, 213, 25)",
         "rgb(255, 213, 25)"
@@ -18,7 +16,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const coresButtons = [
 
-        "rgb(31, 173, 255)",
         "rgb(255, 213, 25)",
         "rgb(255, 213, 25)",
         "rgb(255, 213, 25)"
@@ -29,16 +26,13 @@ document.addEventListener("DOMContentLoaded", () => {
     folhetos.forEach((folheto, index) => {
         const div = document.createElement("div");
         div.classList.add("div-folhetos");
-
         const corDoButton = coresButtons[index % coresFolhetos.length];
-
         div.innerHTML = `
             <h1 class="text-folhetos">${folheto.data}</h1>
             <a href="folhetos/${folheto.arquivo}" target="_blank">
                 <button class="button-folhetos" style="background-color: ${corDoButton};">Baixar</button>
             </a>
         `;
-
         container.appendChild(div);
     });
     document.querySelector("#folhetosContent .content").classList.remove("hidden");
