3c5b5d9e3a9360f1f095cd6340cf86c0f69f3d56
Com.SantaGemmaGalgani-acolitosecoroinhas <acolitosecoroinhascsgg@gmail.com>
2026-04-27 19:30:42 -0300

    Update folhetos.js

--- scripts/folhetos.js
+++ scripts/folhetos.js
@@ -2,23 +2,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const folhetos = [
         
-        { data: "12-Abr-26", arquivo: "12-abr-26.pdf" },
-        { data: "19-Abr-26", arquivo: "19-abr-26.pdf" },
-        { data: "26-Abr-26", arquivo: "26-abr-26.pdf" },
+        { data: "03-Mai-26", arquivo: "3-mai-26.pdf" },
+        { data: "10-Mai-26", arquivo: "10-mai-26.pdf" },
+        { data: "17-Mai-26", arquivo: "17-mai-26.pdf" },
+        { data: "23-Mai-26", arquivo: "23-mai-26.pdf" },
+        { data: "24-Mai-26", arquivo: "24-mai-26.pdf" },
+        { data: "31-Mai-26", arquivo: "31-mai-26.pdf" },
     ];
 
     const coresFolhetos = [
 
         "rgb(255, 213, 25)",
         "rgb(255, 213, 25)",
-        "rgb(255, 213, 25)"
+        "rgb(255, 213, 25)",
+        "rgb(255, 25, 25)",
+        "rgb(255, 25, 25)",
+        "rgb(255, 213, 25)",
     ];
 
     const coresButtons = [
 
         "rgb(255, 213, 25)",
         "rgb(255, 213, 25)",
-        "rgb(255, 213, 25)"
+        "rgb(255, 213, 25)",
+        "rgb(255, 25, 25)",
+        "rgb(255, 25, 25)",
+        "rgb(255, 213, 25)",
     ];
 
     const container = document.getElementById("folhetosContainer");
